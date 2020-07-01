9f3ee12d25c176e44b475f4c5b14d49224f4c125
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-07-01 16:28:17 -0300

    Add nested serializers

--- django_school/classroom/serializers.py
+++ django_school/classroom/serializers.py
@@ -1,8 +1,22 @@
-from classroom.models import Quiz
+from classroom.models import Quiz, Subject, User
 from rest_framework import serializers
 
 
+class SubjectSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Subject
+    fields = ['name', 'color']
+
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_student', 'is_teacher']
+
+
 class QuizSerializer(serializers.ModelSerializer):
+    subject = SubjectSerializer()
+    owner = UserSerializer()
     class Meta:
         model = Quiz
         fields = ['owner', 'name', 'subject']
